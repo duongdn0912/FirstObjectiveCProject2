4e30a8d14cd1800f89e8648332fbd62e5e0827b2
duongdn0912 <duongdn0912@gmail.com>
2017-11-19 23:01:19 +0900

    this commit is to add NewFileOne file

---
+++ FirstOjectiveCApp/NewFileOne.m
@@ -0,0 +1,9 @@
+//
+//  new file one.m
+//  FirstOjectiveCApp
+//
+//  Created by Dinh Ngoc Duong on 2017/11/19.
+//  Copyright © 2017 Dinh Ngoc Duong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
